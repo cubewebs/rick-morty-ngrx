503056fa858f1f727439bc89b6363f8a970ec941
Juan Jose <cubewebs.com@gmail.com>
2022-10-06 11:30:43 +0100

    Next commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterListComponent } from './rick-morty/character-list/character-list.component';
+import { CharacterComponent } from './rick-morty/character/character.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'character-list',
+    component: CharacterListComponent
+  },
+  {
+    path: 'character/:id',
+    component: CharacterComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'character-list',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer, Subscriber, Subscription, tap } from 'rxjs';
+import { SharedService } from './services/shared.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'curso-rxjs';
+export class AppComponent implements OnInit {
+  title = 'rxjs - ngrx';
+  parrafo: string = '';
+  characters: any;
+  
+
+
+  constructor(
+    private ss: SharedService
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    this.ss.getCharacters().pipe(
+      tap( rp => console.log('rp', rp))
+    ).subscribe(
+      resp => this.characters = resp
+    )
+  }
+
 }
